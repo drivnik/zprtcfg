8bbd10e56019a0312459607a2214ba966d009758
Ivan <44730065+drivnik@users.noreply.github.com>
2024-09-22 19:33:49 +0300

    Update get_update_git.sh

--- get_update_git.sh
+++ get_update_git.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-
-git fetch
-cd /opt/zapret/zprtcfg && git pull --show-forced-updates
-/opt/zapret/init.d/openwrt/zapret stop
-opkg update
-opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade  
-cd /opt/zapret && git reset --hard HEAD && git pull --rebase --autostash
+if [[cd /opt/zapret/zprtcfg && git checkout -f != "Your branch is up to date with 'origin/main'."]]
+then
 cd /opt/zapret/zprtcfg && git reset --hard HEAD && git pull --rebase --autostash
-/opt/zapret/ipset/clear_lists.sh
-/opt/zapret/ipset/get_user.sh
-/opt/zapret/ipset/get_config.sh
-/opt/zapret/init.d/openwrt/zapret start
+fi
+
+#/opt/zapret/init.d/openwrt/zapret stop
+#opkg update
+#opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade  
+#cd /opt/zapret && git reset --hard HEAD && git pull --rebase --autostash
+#cd /opt/zapret/zprtcfg && git reset --hard HEAD && git pull --rebase --autostash
+#/opt/zapret/ipset/clear_lists.sh
+#/opt/zapret/ipset/get_user.sh
+#/opt/zapret/ipset/get_config.sh
+#/opt/zapret/init.d/openwrt/zapret start
