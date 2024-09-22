43324ceee1b375cdfe2ccccf5e3b8fe0c1b6f7ef
Ivan <44730065+drivnik@users.noreply.github.com>
2024-09-22 17:06:10 +0300

    Update get_update.sh

--- get_update.sh
+++ get_update.sh
@@ -8,6 +8,7 @@ IPSET_DIR="$(cd "$IPSET_DIR"; pwd)"
 opkg update
 opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade  
 cd /opt/zapret && git reset --hard HEAD && git pull --rebase --autostash
+cd /opt/zapret/zprtcfg && git reset --hard HEAD && git pull --rebase --autostash
 /opt/zapret/ipset/clear_lists.sh
 /opt/zapret/ipset/get_user.sh
 /opt/zapret/ipset/get_config.sh
