e4b9177f6c199c93585bc96b9599dbc810a1b503
Ivan <44730065+drivnik@users.noreply.github.com>
2024-11-01 23:28:08 +0300

    Update get_update_git.sh

--- get_update_git.sh
+++ get_update_git.sh
@@ -2,12 +2,12 @@
 
 cd /opt/zapret/zprtcfg && git reset --hard HEAD && git pull --rebase --autostash
 
-#/opt/zapret/init.d/openwrt/zapret stop
-#opkg update
-#opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade  
-#cd /opt/zapret && git reset --hard HEAD && git pull --rebase --autostash
-#/opt/zapret/ipset/clear_lists.sh
-#/opt/zapret/ipset/get_user.sh
-#/opt/zapret/ipset/get_config.sh
-#/opt/zapret/init.d/openwrt/zapret start
-#/opt/zapret/init.d/openwrt/zapret restart
+/opt/zapret/init.d/openwrt/zapret stop
+opkg update
+opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade  
+cd /opt/zapret && git reset --hard HEAD && git pull --rebase --autostash
+/opt/zapret/ipset/clear_lists.sh
+/opt/zapret/ipset/get_user.sh
+/opt/zapret/ipset/get_config.sh
+/opt/zapret/init.d/openwrt/zapret start
+/opt/zapret/init.d/openwrt/zapret restart
