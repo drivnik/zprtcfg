4209d8b05df9a67f53f3fb87fc67edcbd871d38d
Ivan <44730065+drivnik@users.noreply.github.com>
2025-03-18 14:38:03 +0300

    Update get_update.sh

--- get_update.sh
+++ get_update.sh
@@ -4,6 +4,8 @@
 #opkg update
 #opkg list-upgradable | cut -f 1 -d ' ' | xargs -r opkg upgrade  
 #cd /opt/zapret && git reset --hard HEAD && git pull --rebase --autostash
+#wget https://github.com/bol-van/zapret/releases/download/v70.3/zapret-v70.3-openwrt-embedded.tar.gz
+#tar xvf  zapret-v70.3-openwrt-embedded.tar.gz
 /opt/zapret/ipset/clear_lists.sh
 /opt/zapret/ipset/get_user.sh
 /opt/zapret/ipset/get_config.sh
